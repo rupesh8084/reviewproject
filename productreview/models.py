1df41f7466e3974288a61e34b15199ad27233751
rupesh8084 <rupeshthapa8084@gmail.com>
2025-02-26 12:47:31 +0545

    secong commit

--- productreview/models.py
+++ productreview/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name  # Display name in Django Admin
+    
 
 class Review(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  
